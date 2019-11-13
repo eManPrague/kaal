2cd95dd530a5a86b602e14f73a2436ef1d4b7933
vsouhrada <v.souhrada@gmail.com>
2019-11-13 15:11:00 +0100

    #3: Koin v2.0.1

--- kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/dialog/BaseDialogFragment.kt
+++ kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/dialog/BaseDialogFragment.kt
@@ -1,31 +1,19 @@
 package cz.eman.kaal.presentation.dialog
 
-import android.os.Bundle
 import androidx.fragment.app.DialogFragment
-import cz.eman.kaal.presentation.di.ScopeAware
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import org.koin.androidx.scope.bindScope
-import org.koin.androidx.scope.getKoin
 
 /**
  * @author vsouhrada (vaclav.souhrada@eman.cz)
  * @see[DialogFragment]
+ * @since 0.1.0
  */
 abstract class BaseDialogFragment : DialogFragment(), CoroutineScope {
 
     override val coroutineContext = Dispatchers.Main + SupervisorJob()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        if (this is ScopeAware) {
-            // bind "custom" scope to component lifecycle
-            bindScope(scope = getKoin().getOrCreateScope(scopeId), event = scopedLifecycleEvent)
-        }
-    }
 
     override fun onDestroy() {
         super.onDestroy()
