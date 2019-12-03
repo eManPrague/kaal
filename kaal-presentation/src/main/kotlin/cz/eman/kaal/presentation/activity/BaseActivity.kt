53033e2f4570d887ea48376034c8cb298ca9b19c
vsouhrada <v.souhrada@gmail.com>
2019-12-03 07:22:44 +0100

    ✨ #5: Add Layout id to Base Fragment and Activity
    #5 https://github.com/eManPrague/kaal/issues/5 Added possibility to define a layout by using a constructor instead of calling standard setContent in Activity or standard layout inflater in the Fragment

--- kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/activity/BaseActivity.kt
+++ kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/activity/BaseActivity.kt
@@ -1,5 +1,6 @@
 package cz.eman.kaal.presentation.activity
 
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,9 +12,13 @@ import kotlinx.coroutines.cancel
  * @see[AppCompatActivity]
  * @since 0.1.0
  */
-abstract class BaseActivity : AppCompatActivity(), CoroutineScope {
+abstract class BaseActivity : AppCompatActivity, CoroutineScope {
 
     override val coroutineContext = Dispatchers.Main + SupervisorJob()
+
+    constructor(): super()
+
+    constructor(@LayoutRes contentLayoutId: Int): super(contentLayoutId)
 
     override fun onDestroy() {
         super.onDestroy()
