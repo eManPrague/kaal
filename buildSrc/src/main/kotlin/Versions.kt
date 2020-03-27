dc3e62b7d8799fc5146d648a33c084bbb5b88e22
vsouhrada <v.souhrada@gmail.com>
2020-03-27 06:41:33 +0100

    ⬆️ Update Kotlin and Coroutines
    Updated Kotlin to v1.3.71 and Coroutines to 1.3.5

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,11 +2,11 @@ object Versions {
     const val appcompat = "1.1.0"
     const val fragment = "1.2.0"
     const val archLifecycle = "2.1.0"
-    const val navigationComponent = "2.1.0"
+    const val navigationComponent = "2.2.0"
     const val constraintLayout = "1.1.3"
 
-    const val kotlin = "1.3.61"
-    const val coroutines = "1.3.3"
+    const val kotlin = "1.3.71"
+    const val coroutines = "1.3.5"
     const val dokka = "0.9.17"
 
     const val gradle = "6.2.2"
