a767366b9bc0edccd172e1870291148db0dcc782
Filip Šmíd <smidf94@gmail.com>
2020-03-31 21:22:09 +0200

    💥 Remove Coroutine scope from classes with lifecycle
    In lifecycle-runtime-ktx Jetpack library of version 2.2.0-alpha01
    has been released LifecycleScope. The LifecycleScope is defined for
    each Lifecycle object and this scope lifetime traces Android lifecycle.
    
    See https://developer.android.com/topic/libraries/architecture/coroutines#lifecyclescope

--- kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/dialog/BaseDialogFragment.kt
+++ kaal-presentation/src/main/kotlin/cz/eman/kaal/presentation/dialog/BaseDialogFragment.kt
@@ -1,22 +1,10 @@
 package cz.eman.kaal.presentation.dialog
 
 import androidx.fragment.app.DialogFragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 
 /**
  * @author vsouhrada (vaclav.souhrada@eman.cz)
  * @see[DialogFragment]
  * @since 0.1.0
  */
-abstract class BaseDialogFragment : DialogFragment(), CoroutineScope {
-
-    override val coroutineContext = Dispatchers.Main + SupervisorJob()
-
-    override fun onDestroy() {
-        super.onDestroy()
-        coroutineContext.cancel()
-    }
-}
+abstract class BaseDialogFragment : DialogFragment()
